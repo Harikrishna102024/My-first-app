c3ab8eab3b48a67963ca6bcf8f764c8339463bfc
Harikrishna102024 <tharikrishna.career@gmail.com>
2025-11-28 19:49:20 +0530

    implimented drak mode option

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AppContext } from '../../appContext';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,21 @@ export class HeaderComponent {
 
   @Input('selectedTitle') selectedTitle: any;
 
-  constructor(){}
+  constructor(public context: AppContext) { }
+
+  toggleTheme(event: any) {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      this.context.theme = 'dark';
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      this.context.theme = 'light';
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+    console.log(this.context.theme)
+  }
+
 
 }
